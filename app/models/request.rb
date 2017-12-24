d7c9de670bd4e9f351e4ebbbaaee6ebcc140e772
Alaap Dhall <alaapdhall79@gmail.com>
2017-12-24 22:10:34 +0530

    pay amount fixed

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,7 +9,7 @@ belongs_to :institute
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 #validates_associated :status, :college,:user
 validates :email , format: {with: VALID_EMAIL_REGEX}
-validates :fullname,:email,:contact,:address_1,:reason,:fathername,:enrollment_no,:batch,:course,:college_id, presence: true
+validates :fullname,:email,:contact,:address_1,:reason,:fathername,:batch,:course,:college_id, presence: true
 mount_uploader :fircopy,FircopyUploader
 
 end
